e49c4ce48bba205ed9f7d7b506226693241cdff6
AYESHAMAZHER906 <ayeshamazher906@gmail.com>
2024-10-23 15:42:46 +0500

    Add files via upload

---
+++ code1.js
@@ -0,0 +1,7 @@
+const accountId = 134;
+let accountEmail = "yasha";
+var accountPassword = "123456";
+accountCity = "karachi";
+
+
+console.table([accountCity,accountEmail,accountPassword,accountId]);
